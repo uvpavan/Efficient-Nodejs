f79dea8f68e4233a9e08a2e681bf4995f2e59163
vpavan <vpavan8747@gmail.com>
2024-04-29 19:10:37 +0530

    simple web server

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    res.end('Hello World \n');
+});
+
+server.listen(3000, () => {
+    console.log('Server is running......');
+})
