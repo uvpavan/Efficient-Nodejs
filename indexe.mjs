e72b0d81831d9c7ecf438d7444007ecbf78c73c9
vpavan <vpavan8747@gmail.com>
2024-04-29 19:30:05 +0530

    added es module export

---
+++ indexe.mjs
@@ -0,0 +1,5 @@
+import app from './servere.mjs';
+
+app.listen(3000, () => {
+    console.log("Server listening on http://locahost:300");
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+
 const server = http.createServer((req, res) => {
 
     res.end('Hello World \n');
